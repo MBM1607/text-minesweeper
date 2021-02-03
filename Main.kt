e1faeeb5e69eda00cab5d3531b500485af9743f5
mbm_1607 <muhammadkhan1607@gmail.com>
2021-02-04 01:36:03 +0500

    Complete Jetbrains Academy requirements
    Modify a lot of the game logic and make it work
    according to specs. Add flood fill algorithm for
    freeing cells and another winning condition under
    all cells other then mines being explored.

--- Main.kt
+++ Main.kt
@@ -1,25 +1,36 @@
 package minesweeper
 
 import kotlin.random.Random
+import kotlin.system.exitProcess
 
 fun main() {
     print("How many mines do you want on the field? ")
     val mineField = MineField(9, 9)
-    mineField.placeMines(readLine()!!.toInt())
+    val numMines = readLine()!!.toInt()
+
+    mineField.placeMines(numMines)
+    mineField.printField()
     println()
-    println(mineField)
 
     while (!mineField.isWin()) {
-        print("Set/delete mine marks (x and y coordinates): ")
+        print("Set/unset mines marks or claim a cell as free: ")
         try {
-            val (j, i) = readLine()!!.split(" ", limit = 2).map { it.toInt() }
-            if (mineField.placeMark(i - 1, j - 1)) {
-                println(mineField)
+            val (jStr, iStr, command) = readLine()!!.split(" ", limit = 3)
+            val i = iStr.toInt() - 1
+            val j = jStr.toInt() - 1
+            if (command == "free") {
+                mineField.free(i , j)
+            } else if (command == "mine") {
+                if (mineField.placeMark(i, j)) {
+                    mineField.printField()
+                } else {
+                    println("This cell is already explored")
+                }
             } else {
-                println("There is a number here!")
+                println("Invalid command")
             }
         } catch (e: Exception) {
-            println("Invalid indices")
+            println("Invalid indices or command")
         }
     }
 
@@ -32,7 +43,7 @@ class MineField(private val rows: Int, private val columns: Int) {
     private val userMarks = mutableListOf<Pair<Int, Int>>()
     private val mines = mutableListOf<Pair<Int, Int>>()
 
-    override fun toString(): String {
+    fun printField(printMines: Boolean = false) {
         var str = " |"
         for (i in 1..columns) str += i.toString()
         str += "|\n"
@@ -43,6 +54,7 @@ class MineField(private val rows: Int, private val columns: Int) {
             str += "${i}|"
             for (j in 1..columns) {
                 str += when {
+                    printMines && Pair(i - 1, j - 1) in mines -> 'X'
                     Pair(i - 1, j - 1) in userMarks -> '*'
                     else -> field[i - 1][j - 1]
                 }
@@ -53,7 +65,7 @@ class MineField(private val rows: Int, private val columns: Int) {
         for (i in 1..columns) str += "-"
         str += "|"
 
-        return str
+        println(str)
     }
 
     fun placeMines(numMines: Int) {
@@ -66,14 +78,74 @@ class MineField(private val rows: Int, private val columns: Int) {
     }
 
     // Place a mark at the index and return true if successful
-    // and false if a number exists on the location
+    // and false if that cell is explored
     fun placeMark(i: Int, j: Int): Boolean {
         when {
-            field[i][j].isDigit() -> return false
+            field[i][j] != '.' -> return false
             Pair(i, j) in userMarks -> userMarks.remove(Pair(i, j))
             else -> userMarks.add(Pair(i, j))
         }
         return true
+    }
+
+    fun free(i: Int, j: Int) {
+        val indices = Pair(i, j)
+        when {
+            indices in mines -> fail()
+            field[i][j] == '.' -> freeCell(i, j)
+            else -> {
+                println("Cell is already free")
+                return
+            }
+        }
+        printField()
+    }
+
+    private fun fail() {
+        printField(true)
+        println("You stepped on a mine and failed!")
+        exitProcess(0)
+    }
+
+    private fun freeCell(i: Int, j: Int) {
+        val indices = Pair(i, j)
+        if (indices in userMarks) userMarks.remove(indices)
+        val mines = countNeighbourMine(i, j)
+        if (mines > 0) {
+            field[i][j] = mines.toString().first()
+        }
+        else if (mines == 0 && field[i][j] == '.') {
+            field[i][j] = '/'
+            if (i != 0) {
+                freeCell(i - 1, j)
+                if (j != 0) freeCell(i - 1, j - 1)
+                if (j != columns - 1) freeCell(i - 1, j + 1)
+            }
+            if (i != rows - 1) {
+                freeCell(i + 1, j)
+                if (j != 0) freeCell(i + 1, j - 1)
+                if (j != columns - 1) freeCell(i + 1, j + 1)
+            }
+            if (j != 0) freeCell(i, j - 1)
+            if (j != columns - 1) freeCell(i, j + 1)
+        }
+    }
+
+    private fun countNeighbourMine(i: Int, j: Int): Int {
+        var result = 0
+        if (i != 0) {
+            if (Pair(i - 1, j) in mines) result++
+            if (j != 0 && Pair(i - 1, j - 1) in mines) result++
+            if (j != columns - 1 && Pair(i - 1, j + 1) in mines) result++
+        }
+        if (i != rows - 1) {
+            if (Pair(i + 1, j) in mines) result++
+            if (j != 0 && Pair(i + 1, j - 1) in mines) result++
+            if (j != columns - 1 && Pair(i + 1, j + 1) in mines) result++
+        }
+        if (j != 0 && Pair(i, j - 1) in mines) result++
+        if (j != columns - 1 && Pair(i, j + 1) in mines) result++
+        return result
     }
 
     private fun placeMine(i: Int, j: Int): Int {
@@ -84,28 +156,15 @@ class MineField(private val rows: Int, private val columns: Int) {
         return 0
     }
 
-    private fun countNeighbourMines(i: Int, j: Int) {
-        if (i != 0) {
-            incrementCell(i - 1, j)
-            if (j != 0) incrementCell(i - 1, j - 1)
-            if (j != columns - 1) incrementCell(i - 1, j + 1)
-        }
-        if (i != rows - 1) {
-            incrementCell(i + 1, j)
-            if (j != 0) incrementCell(i + 1, j - 1)
-            if (j != columns - 1) incrementCell(i + 1, j + 1)
-        }
-        if (j != 0) incrementCell(i, j - 1)
-        if (j != columns - 1) incrementCell(i, j + 1)
+    private fun allExplored(): Boolean {
+        for ((i, row) in field.withIndex())
+            for((j, cell) in row.withIndex())
+                if (cell == '.' && Pair(i, j) !in mines)
+                    return false
+        return true
     }
 
-    // Increase the neighbour count of the cell
-    private fun incrementCell(i: Int, j: Int) {
-        if (field[i][j].isDigit()) field[i][j] = (field[i][j].toInt() + 1).toChar()
-        else if (Pair(i, j) in mines) field[i][j] = '1'
-    }
-
-    fun isWin(): Boolean {
+    private fun allMinesMarked(): Boolean {
         for ((i, row) in field.withIndex())
             for (j in row.indices) {
                 val indices = Pair(i, j)
@@ -114,5 +173,9 @@ class MineField(private val rows: Int, private val columns: Int) {
                     return false
             }
         return true
+    }
+
+    fun isWin(): Boolean {
+        return allMinesMarked() || allExplored()
     }
 }
